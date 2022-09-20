d65a0b67daa7daad19ab4b147e78ceaa6606d543
Talita Saez <tltsaez@gmail.com>
2022-09-20 18:39:29 -0300

    resolução requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,23 @@
-// test('Teste o componente <About.js />', () => {
-//  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
 
-// });
-// });
-// //
+describe('Teste o componente <About.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const about = screen.getByRole('heading', {
+      name: /about pokédex/i,
+    });
+    expect(about).toBeInTheDocument();
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img', {
+      name: /pokédex/i,
+    });
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
