6d5c4d5136f6cc9af361912dc110a1fd7db6a5f4
Talita Saez <tltsaez@gmail.com>
2022-09-20 18:54:15 -0300

    resolução requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <About.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favoritePk = screen.getByText(/no favorite pokemon found/i);
+    expect(favoritePk).toBeInTheDocument();
+  });
+});
