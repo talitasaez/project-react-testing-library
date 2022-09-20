3996e6af8396bfb986483ce73747fa6947824016
Talita Saez <tltsaez@gmail.com>
2022-09-20 16:59:37 -0300

    arrumei erros de lint req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,36 +6,47 @@ import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Link do topo da aplicação', () => {
-  // eslint-disable-next-line max-len
-  test('Teste se a aplicação é redirecionada para a página inicial ao clicar no link Home', () => {
-    const { history } = renderWithRouter(<App />);
+  test(
+    'Teste se a aplicação é redirecionada para a página inicial ao clicar no link Home',
+    () => {
+      const { history } = renderWithRouter(<App />);
 
-    const homeLink = screen.getByRole('link', { name: /Home/i });
+      const homeLink = screen.getByRole('link', { name: /Home/i });
 
-    expect(homeLink).toBeInTheDocument();
-    userEvent.click(homeLink);
-    expect(history.location.pathname).toBe('/');
-  });
-  // eslint-disable-next-line max-len
-  test('Teste se a aplicação é redirecionada para a página About  ao clicar no About', () => {
-    const { history } = renderWithRouter(<App />);
+      expect(homeLink).toBeInTheDocument();
+      userEvent.click(homeLink);
+      expect(history.location.pathname).toBe('/');
+    },
+  );
 
-    const aboutLink = screen.getByRole('link', { name: /About/i });
+  test(
+    'Teste se a aplicação é redirecionada para a página About  ao clicar no About',
+    () => {
+      const { history } = renderWithRouter(<App />);
 
-    expect(aboutLink).toBeInTheDocument();
-    userEvent.click(aboutLink);
-    expect(history.location.pathname).toBe('/about');
-  });
-  // eslint-disable-next-line max-len
-  test('Teste se a aplicação é redirecionada para a página Pokémons Favoritados ao clicar no Favorite Pokémons', () => {
-    const { history } = renderWithRouter(<App />);
+      const aboutLink = screen.getByRole('link', { name: /About/i });
 
-    const favoritePokemonLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+      expect(aboutLink).toBeInTheDocument();
+      userEvent.click(aboutLink);
+      expect(history.location.pathname).toBe('/about');
+    },
+  );
 
-    expect(favoritePokemonLink).toBeInTheDocument();
-    userEvent.click(favoritePokemonLink);
-    expect(history.location.pathname).toBe('/favorites');
-  });
+  test(
+    'Teste se a aplicação é redirecionada Pokémons Favoritados ao clicar no Favorite',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const favoritePokemonLink = screen.getByRole(
+        'link',
+        { name: /Favorite Pokémons/i },
+      );
+
+      expect(favoritePokemonLink).toBeInTheDocument();
+      userEvent.click(favoritePokemonLink);
+      expect(history.location.pathname).toBe('/favorites');
+    },
+  );
   // eslint-disable-next-line max-len
   test('Teste se a aplicação é redirecionada para a página Not Found  ao entrar em uma URL desconhecida.', () => {
     const { history } = renderWithRouter(<App />);
